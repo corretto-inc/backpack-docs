0cfed63cd1a9708a250968000e85899914c6231e
Wooyoung Tyler Kim <wooyoung.kim@corretto.io>
2019-09-10 19:09:03 +0900

    asdf

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,48 +11,46 @@
 // List of projects/orgs using your project for the users page.
 const users = [
   {
-    caption: 'User1',
+    caption: "User1",
     // You will need to prepend the image path with your baseUrl
     // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    image: "/img/undraw_open_source.svg",
+    infoLink: "https://www.facebook.com",
+    pinned: true
+  }
 ];
 
 const siteConfig = {
-  title: 'Backpack', // Title for your website.
-  tagline: 'Backpack docs',
-  url: 'https://corretto-inc.github.io', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: "백엔드패키지 - 사용자매뉴얼", // Title for your website.
+  tagline: "Backpack docs",
+  url: "https://corretto-inc.github.io", // Your website URL
+  baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'backpack-docs',
-  organizationName: 'Corretto, Inc.',
+  projectName: "backpack-docs",
+  organizationName: "Corretto, Inc.",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'getting-started-overview', label: 'Docs'},
-  ],
+  headerLinks: [{ doc: "getting-started-overview", label: "Docs" }],
 
   // If you have users set above, you add it here:
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: "img/favicon.ico",
+  footerIcon: "img/favicon.ico",
+  favicon: "img/favicon.ico",
 
   /* Colors for website */
   colors: {
-    primaryColor: '#1e5284',
-    secondaryColor: '#492c0c',
+    primaryColor: "#1e5284",
+    secondaryColor: "#492c0c"
   },
 
   /* Custom fonts for website */
@@ -74,20 +72,20 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
+    theme: "default"
   },
 
   // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+  scripts: ["https://buttons.github.io/buttons.js"],
 
   // On page navigation for the current documentation page.
-  onPageNav: 'separate',
+  onPageNav: "separate",
   // No .html extensions for paths.
   cleanUrl: true,
 
   // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+  ogImage: "img/undraw_online.svg",
+  twitterImage: "img/undraw_tweetstorm.svg",
 
   // For sites with a sizable amount of content, set collapsible to true.
   // Expand/collapse the links and subcategories under categories.
@@ -97,7 +95,7 @@ const siteConfig = {
   enableUpdateBy: true,
 
   // Show documentation's last update time.
-  enableUpdateTime: true,
+  enableUpdateTime: true
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
